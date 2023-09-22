5cb8e9da36db36f0c683a4e84ed88598ada12d78
Oumar Siby <oumarsiby10@gmail.com>
2023-09-22 19:39:24 -0400

    Made join interactive.

--- src/features/project/projectPage/ProjectPageContainer.js
+++ src/features/project/projectPage/ProjectPageContainer.js
@@ -2,14 +2,16 @@ import ProjectPageView from "./ProjectPageView";
 import { useNavigate } from "react-router-dom";
 import { useGetProjectQuery, useGetMembersQuery, useAddRequestMutation } from "../../api/apiSlice";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 
 export function ProjectPageContainer() {
   const navigate = useNavigate();
   let params = useParams();
+  const [setJoined, joined] = useState(false);
   const { data, error, isLoading } = useGetProjectQuery(params.projectId);
   const {data: requests, isError, loading, isSuccess } = useGetMembersQuery(params.projectId)
-  const [addRequest, {isSuccess: isSuccessRequest}] = useAddRequestMutation()
+  const [addRequest, {isSuccess: isSuccessRequest, isLoading: isLoadingRequest, isError: isErrorRequest}] = useAddRequestMutation()
   
 
   if (isLoading || loading) {
@@ -26,14 +28,21 @@ export function ProjectPageContainer() {
 
   const handleJoin = async () => {
     await addRequest(params.projectId);
+    if (isLoadingRequest) {
+      return <div>Sending Request...</div>;
+    }
+  
+    if (isErrorRequest) {
+      return <div>Error Sending Request: {error.message}</div>;
+    }
     if(isSuccessRequest){
-      navigate('/requests')
+      setJoined(true);
     }
 
   }
   if(isSuccess)
   {return (
-    <ProjectPageView project={data} members={requests} joinHandler={handleJoin} />
+    <ProjectPageView project={data} members={requests} joinHandler={handleJoin} isJoined = {joined} />
   )}
 }
   
--- src/features/project/projectPage/ProjectPageView.js
+++ src/features/project/projectPage/ProjectPageView.js
@@ -1,6 +1,9 @@
 import Members from "./Members";
 const ProjectPageView = (props) => {
     console.log(props.members)
+    if(props.isJoined){
+        return <>Your requst has been sent</>
+    }
     return (
         <div className="container col-6 my-5" style={{minHeight: "100vh"}}>
             <h1>{props.project.projectTitle}</h1>
@@ -8,7 +11,7 @@ const ProjectPageView = (props) => {
             <p>Owner: {props.project.owner.firstName} {props.project.owner.lastName}</p>
             <h5>Members</h5>
             <Members members={props.members}/>
-            <button onClick={props.joinHandler} className="btn btn-success d-grid col-3 mx-auto my-4">Join</button>
+            <button type="button" onClick={props.joinHandler} className="btn btn-success d-grid col-3 mx-auto my-4">Join</button>
         </div>
     )
 }
